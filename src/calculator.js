ecb98fa250657612524f8068e09b72fb4d6719c5
Ashish G Bhargav <ashishgbhargav@gmail.com>
2026-02-20 04:17:57 +0530

    Delete src/calculator.js

--- src/calculator.js
+++
@@ -1,9 +0,0 @@
-// src/calculator.js
-
-function calculateTotal(subtotal, taxRate) {
-    // LOGIC ERROR: It is dividing the subtotal by the tax rate instead of multiplying
-    const tax = subtotal / taxRate; 
-    return subtotal + tax
-}
-
-module.exports = { calculateTotal };
