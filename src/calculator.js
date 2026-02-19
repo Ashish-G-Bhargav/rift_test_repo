eb38ef4347349c91190ef3e9cac478a59da31e7b
Bhuvan Tharanath <bhuvant3726@gmail.com>
2026-02-19 20:57:18 +0530

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -3,7 +3,7 @@
 function calculateTotal(subtotal, taxRate) {
     // LOGIC ERROR: It is dividing the subtotal by the tax rate instead of multiplying
     const tax = subtotal / taxRate; 
-    return subtotal + tax;
+    return subtotal + tax
 }
 
 module.exports = { calculateTotal };
